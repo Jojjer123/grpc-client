1830844010693196a3c4414c43c43b4d18781823
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-03 11:45:05 +0100

    Add concurrency for requests.

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	adminServer "github.com/onosproject/grpc-client/cmd/generated"
 	"google.golang.org/grpc"
@@ -11,9 +12,12 @@ import (
 const numberOfComponents = 7
 
 func main() {
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+
 	var conn *grpc.ClientConn
 
-	conn, err := grpc.Dial("10.244.0.19:4040", grpc.WithInsecure())
+	conn, err := grpc.Dial("10.244.0.16:4040", grpc.WithInsecure())
 
 	if err != nil {
 		fmt.Println("Did not connect: ", err)
@@ -22,8 +26,17 @@ func main() {
 	defer conn.Close()
 
 	c := adminServer.NewMonitorAdminInterfaceClient(conn)
+	go sendRequest(c, "supa action", "supa target", &waitGroup)
 
-	response, err := c.MonitorDevice(context.Background(), &adminServer.MonitorMessage{Action: "supa-action", Target: "supa-server"})
+	c2 := adminServer.NewMonitorAdminInterfaceClient(conn)
+	go sendRequest(c2, "action in uganda", "target acquired", &waitGroup)
+
+	waitGroup.Wait()
+}
+
+func sendRequest(c adminServer.MonitorAdminInterfaceClient, action string, target string, waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+	response, err := c.MonitorDevice(context.Background(), &adminServer.MonitorMessage{Action: action, Target: target})
 	if err != nil {
 		fmt.Println("Error when calling MonitorDevice: ", err)
 	}
