6c80238dc043c03089086196a4597126b3a0e48e
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-15 08:22:56 +0100

    Add set request functionality.

--- cmd/main.go
+++ cmd/main.go
@@ -9,10 +9,73 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/openconfig/gnmi/client"
 	gclient "github.com/openconfig/gnmi/client/gnmi"
-	// gpb "github.com/openconfig/gnmi/proto/gnmi"
+	pb "github.com/openconfig/gnmi/proto/gnmi"
 )
 
 func main() {
+	set()
+}
+
+func set() {
+	ctx := context.Background()
+
+	address := []string{"device-monitor:11161"}
+
+	c, err := gclient.New(ctx, client.Destination{
+		Addrs:       address,
+		Target:      "device-monitor",
+		Timeout:     time.Second * 5,
+		Credentials: nil,
+		TLS:         nil,
+	})
+
+	if err != nil {
+		// fmt.Errorf("could not create a gNMI client: %v", err)
+		fmt.Print("Could not create a gNMI client: ")
+		fmt.Println(err)
+	}
+
+	var updateList []*pb.Update
+
+	data := pb.TypedValue_StringVal{
+		StringVal: "set request data here",
+	}
+
+	// TODO: REWORK, could place it all in Path
+	update := pb.Update{
+		Path: &pb.Path{
+			Target: "switch 123",
+		},
+		Val: &pb.TypedValue{
+			Value: &data,
+		},
+	}
+
+	updateList = append(updateList, &update)
+
+	setRequest := pb.SetRequest{
+		Update: updateList,
+	}
+
+	response, err := c.(*gclient.Client).Set(ctx, &setRequest)
+
+	fmt.Print("Response from device-monitor is: ")
+	fmt.Println(response)
+
+	if err != nil {
+		fmt.Print("Target returned RPC error for Set: ")
+		fmt.Println(err)
+	}
+
+	fmt.Println("Client connected successfully")
+
+	for {
+		// fmt.Println(c.(*gclient.Client).Recv())
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func sub() {
 	ctx := context.Background()
 
 	address := []string{"device-monitor:11161"}
