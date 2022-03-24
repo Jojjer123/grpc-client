dd22e06fd28e706c33eabbd3da7cce691674435e
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-24 20:19:07 +0100

    Add gnmi request with converted XML.
    Add request of converted XML and try to build a tree structure from it.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"time"
@@ -26,35 +27,215 @@ func main() {
 	// 	Path string
 	// }
 
-	var config0 []types.DeviceCounters //struct
+	// COMMENTED FOR TESTING OF XML CONVERSION
+	// var config0 []types.DeviceCounters //struct
 
-	c := types.DeviceCounters{
-		Name:     "second",
-		Interval: 123,
-		Path:     "elem: <name: 'test'>",
-	}
+	// c := types.DeviceCounters{
+	// 	Name:     "second",
+	// 	Interval: 123,
+	// 	Path:     "elem: <name: 'test'>",
+	// }
 
-	config0 = append(config0, c)
-	// config0[0].Name = "second"
-	// config0[0].Interval = 123
-	// config0[0].Path = "elem: <name: 'test'>"
+	// config0 = append(config0, c)
+	// // config0[0].Name = "second"
+	// // config0[0].Interval = 123
+	// // config0[0].Path = "elem: <name: 'test'>"
 
-	configs := []types.Conf{
-		types.Conf{
-			Counter: config0,
-		},
-	}
+	// configs := []types.Conf{
+	// 	types.Conf{
+	// 		Counter: config0,
+	// 	},
+	// }
 
-	conf := types.ConfigRequest{
-		DeviceIP: "192.168.1.82",
-		Configs:  configs,
-	}
+	// conf := types.ConfigRequest{
+	// 	DeviceIP: "192.168.1.82",
+	// 	Configs:  configs,
+	// }
 
-	setUpdate(conf)
+	// setUpdate(conf)
+
+	testing()
 
 	for {
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func testing() {
+	ctx := context.Background()
+
+	address := []string{"storage-service:11161"}
+
+	c, err := gclient.New(ctx, client.Destination{
+		Addrs:       address,
+		Target:      "storage-service",
+		Timeout:     time.Second * 5,
+		Credentials: nil,
+		TLS:         nil,
+	})
+
+	if err != nil {
+		// fmt.Errorf("could not create a gNMI client: %v", err)
+		fmt.Print("Could not create a gNMI client: ")
+		fmt.Println(err)
+	}
+
+	// var updateList []*pb.Update
+
+	// data := pb.TypedValue_StringVal{
+	// 	StringVal: "Create", // Set command
+	// }
+
+	// actionMap := make(map[string]string)
+	// actionMap["Action"] = action
+
+	// pathElements := []*pb.PathElem{}
+
+	// pathElements = append(pathElements, &pb.PathElem{
+	// 	Name: "Action",
+	// 	// Key:  actionMap,
+	// })
+
+	// configMap := make(map[string]string)
+	// configMap["ConfigIndex"] = confIndex
+
+	// pathElements = append(pathElements, &pb.PathElem{
+	// 	Name: "ConfigIndex",
+	// 	// Key:  configMap,
+	// })
+
+	// // TODO: REWORK, could place it all in Path
+	// update := pb.Update{
+	// 	Path: &pb.Path{
+	// 		Elem:   pathElements,
+	// 	},
+	// 	// Val: &pb.TypedValue{
+	// 	// 	Value: &data,
+	// 	// },
+	// }
+
+	// updateList = append(updateList, &update)
+
+	getRequest := pb.GetRequest{}
+
+	response, err := c.(*gclient.Client).Get(ctx, &getRequest)
+	if err != nil {
+		fmt.Print("Target returned RPC error for Testing: ")
+		fmt.Println(err)
+	}
+
+	// fmt.Print("Response from storage-service is: ")
+	// fmt.Println(response)
+
+	// schema := Schema{}
+	// dec := gob.NewDecoder(bytes.NewReader(response.Notification[0].Update[0].Val.GetBytesVal()))
+	// err = dec.Decode(&schema)
+	// if err != nil {
+	// 	fmt.Println("Failed to decode byte slice!")
+	// 	fmt.Println(err)
+	// }
+
+	var schema Schema
+
+	json.Unmarshal(response.Notification[0].Update[0].Val.GetBytesVal(), &schema)
+
+	// fmt.Println(schema)
+	getTreeStructure(schema)
+	// schemaTree := getTreeStructure(schema)
+
+	// fmt.Println(schemaTree)
+
+	// fmt.Println("Client connected successfully")
+}
+
+// TODO: add pointer that traverse the tree based on tags, use that pointer to
+// get correct parents.
+func getTreeStructure(schema Schema) *SchemaTree {
+	tree := &SchemaTree{}
+	for index, entry := range schema.Entries {
+		fmt.Println("-------------------")
+		if index == 0 {
+			// fmt.Println("in data")
+			tree.Name = entry.Name
+			tree.Namespace = entry.Namespace
+			fmt.Println(tree.Name)
+			tree = &SchemaTree{Parent: tree}
+			continue
+		}
+		if entry.Value == "" { //&& schema.Entries[index + 1].ParentName  { // Directory
+			if entry.Tag == "end" {
+				// tree = tree.Parent
+				continue
+			}
+			// fmt.Println("in dir")
+			tree.Name = entry.Name
+			tree.Namespace = entry.Namespace
+			tree.Parent.Children = append(tree.Parent.Children, tree)
+
+			fmt.Println(tree.Name)
+			// fmt.Println(tree.Parent.Name)
+			// fmt.Println(tree)
+
+			tree = &SchemaTree{Parent: tree}
+		} else { // Leaf
+			// fmt.Println("in leaf")
+			tree.Name = entry.Name
+			tree.Value = entry.Value
+			tree.Parent.Children = append(tree.Parent.Children, tree)
+			fmt.Println(tree.Name)
+			fmt.Println(tree.Value)
+			// fmt.Println(tree.Parent.Name)
+			// tree = tree.Parent
+		}
+		// fmt.Println("-------------------")
+		// fmt.Println(entry)
+		// fmt.Println("###################")
+	}
+	return tree
+}
+
+// func (s *Schema) UnmarshalBinary(data []byte) (err error) {
+// 	dec := gob.NewDecoder(bytes.NewReader(data))
+// 	if err = dec.Decode(&s.Entry); err != nil {
+// 		return
+// 	}
+// 	if err = dec.Decode(&s.Children); err != nil {
+// 		return
+// 	}
+// 	// var isCyclic bool
+// 	// if err = dec.Decode(&isCyclic); err != nil {
+// 	//     return
+// 	// }
+// 	// err = dec.Decode(&p.Q)
+// 	// if isCyclic {
+// 	//     p.Q.P = p
+// 	// }
+// 	return
+// }
+
+// type Schema struct {
+// 	Entry    *SchemaEntry
+// 	Children map[string]interface{}
+// 	Parent   *Schema
+// }
+
+type SchemaTree struct {
+	Name      string
+	Namespace string
+	Children  []*SchemaTree
+	Parent    *SchemaTree
+	Value     string
+}
+
+type Schema struct {
+	Entries []SchemaEntry
+}
+
+type SchemaEntry struct {
+	Name      string
+	Tag       string
+	Namespace string
+	Value     string
 }
 
 func setCreate(action string, target string, confIndex string) {
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.5.2
 	github.com/openconfig/gnmi v0.0.0-20220131173555-39aa74195f0d
+	github.com/openconfig/goyang v0.0.0-20200115183954-d0a48929f0ea
+	github.com/pborman/getopt v1.1.0 // indirect
 	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1 // indirect
