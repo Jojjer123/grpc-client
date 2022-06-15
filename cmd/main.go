1ef47c9cc1497a77babab62ad4ffa21a507ca768
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-06-15 21:12:23 +0200

    Print delay for get function.

--- cmd/main.go
+++ cmd/main.go
@@ -50,7 +50,12 @@ func main() {
 	capabilities := netconf.DefaultCapabilities
 	s.Transport.SendHello(&netconf.HelloMessage{Capabilities: capabilities})
 
+	start := time.Now().UnixNano()
 	reply, err := s.Exec(netconf.RawMethod("<get><filter type='subtree'><interfaces xmlns='urn:ietf:params:xml:ns:yang:ietf-interfaces'><interface><name>sw0p1</name><ethernet xmlns='urn:ieee:std:802.3:yang:ieee802-ethernet-interface'><statistics><frame><in-total-frames></in-total-frames></frame></statistics></ethernet></interface></interfaces></filter></get>"))
+	end := time.Now().UnixNano()
+
+	fmt.Printf("Delay: %v", end-start)
+
 	if err != nil {
 		panic(err)
 	}
