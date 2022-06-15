64f771b60efc6a04cb936d158225e1c60fdfc81e
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-06-15 21:59:55 +0200

    Receive hello message before sending hello.

--- cmd/main.go
+++ cmd/main.go
@@ -47,9 +47,6 @@ func main() {
 	// fmt.Println(s.ServerCapabilities)
 	// fmt.Println(s.SessionID)
 
-	capabilities := netconf.DefaultCapabilities
-	s.Transport.SendHello(&netconf.HelloMessage{Capabilities: capabilities})
-
 	var hello_reply *netconf.HelloMessage
 	hello_reply, err = s.Transport.ReceiveHello()
 	if err != nil {
@@ -57,6 +54,9 @@ func main() {
 	}
 
 	fmt.Printf("Hello received is: %v", hello_reply)
+
+	capabilities := netconf.DefaultCapabilities
+	s.Transport.SendHello(&netconf.HelloMessage{Capabilities: capabilities})
 
 	start := time.Now().UnixNano()
 	reply, err := s.Exec(netconf.RawMethod("<get><filter type='subtree'><interfaces xmlns='urn:ietf:params:xml:ns:yang:ietf-interfaces'><interface><name>sw0p1</name><ethernet xmlns='urn:ieee:std:802.3:yang:ieee802-ethernet-interface'><statistics><frame><in-total-frames></in-total-frames></frame></statistics></ethernet></interface></interfaces></filter></get>"))
